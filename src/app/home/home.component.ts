9774d0e073e49cee46664a20ef73d74f12ceabbe
EddyLucany <eddylucandy@icloud.com>
2025-05-10 22:51:33 +0200

    cambios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
+  standalone: true,
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  newsItems = [
+    { title: 'Item 1', description: 'Exceptional efficient emerging, minimum viable content curated...', image: '/img/news.PNG' },
+    { title: 'Item 2', description: 'Gimza conversation exquisite perfect, nostalgic intricate content...', image: '/img/2147775681-2.jpg' },
+    { title: 'Item 3', description: 'International first-class nulle, qui...', image: '/img/news.PNG' }
+  ];
 
+  reviews = [
+    { title: 'Subheading', description: 'Body text for whatever you’d like to add more to the subheading.', image: '/img/news.PNG' },
+    { title: 'Subheading', description: 'Body text for whatever you’d like to expand on the main point.', image: '/img/2147775681-2.jpg' },
+    { title: 'Subheading', description: 'Body text for whatever you’d like to share more.', image: '/img/2147775681-2.jpg' }
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void { }
 }

e6689451a1868ac8d9e5819c13c01759032e1773
EddyLucany <eddylucandy@icloud.com>
2025-05-11 23:21:40 +0200

    cambios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
-  templateUrl: './home.component.html',
   standalone: true,
-  styleUrl: './home.component.css'
+  imports: [CommonModule],
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
   newsItems = [
-    { title: 'Item 1', description: 'Exceptional efficient emerging, minimum viable content curated...', image: '/img/news.PNG' },
+    { title: 'Novedades del Año', description: 'Exceptional efficient emerging, minimum viable content curated...', image: '/img/news.PNG' },
     { title: 'Item 2', description: 'Gimza conversation exquisite perfect, nostalgic intricate content...', image: '/img/2147775681-2.jpg' },
     { title: 'Item 3', description: 'International first-class nulle, qui...', image: '/img/news.PNG' }
   ];
@@ -20,7 +21,17 @@ export class HomeComponent implements OnInit {
     { title: 'Subheading', description: 'Body text for whatever you’d like to share more.', image: '/img/2147775681-2.jpg' }
   ];
 
+  currentNewsIndex = 0;
+
   constructor() { }
 
   ngOnInit(): void { }
+
+  nextNews(): void {
+    this.currentNewsIndex = (this.currentNewsIndex + 1) % this.newsItems.length;
+  }
+
+  prevNews(): void {
+    this.currentNewsIndex = (this.currentNewsIndex - 1 + this.newsItems.length) % this.newsItems.length;
+  }
 }

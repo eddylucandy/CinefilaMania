e05a6eeb5134bfb9ce7ea6ef936778eb06b9a956
EddyLucany <eddylucandy@icloud.com>
2025-05-13 23:03:59 +0200

    cambios

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -10,15 +10,15 @@ import { CommonModule } from '@angular/common';
 })
 export class HomeComponent implements OnInit {
   newsItems = [
-    { title: 'Novedades del Año', description: 'Exceptional efficient emerging, minimum viable content curated...', image: '/img/news.PNG' },
-    { title: 'Item 2', description: 'Gimza conversation exquisite perfect, nostalgic intricate content...', image: '/img/2147775681-2.jpg' },
-    { title: 'Item 3', description: 'International first-class nulle, qui...', image: '/img/news.PNG' }
+    { title: 'Novedades del Año', description: 'Exceptional efficient emerging, minimum viable content curated...', image: 'https://wallpapers.com/images/featured/movie-9pvmdtvz4cb0xl37.jpg' },
+    { title: 'Item 2', description: 'Gimza conversation exquisite perfect, nostalgic intricate content...', image: 'https://preview.redd.it/xe2175thl6u81.jpg?auto=webp&s=5842f2df12adb095e635f43e809d2f38093e7a13' },
+    { title: 'Item 3', description: 'International first-class nulle, qui...', image: 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/2NJO3FE7QBHQJD5GKNCI6XIODM.jpg' }
   ];
 
   reviews = [
-    { title: 'Subheading', description: 'Body text for whatever you’d like to add more to the subheading.', image: '/img/news.PNG' },
-    { title: 'Subheading', description: 'Body text for whatever you’d like to expand on the main point.', image: '/img/2147775681-2.jpg' },
-    { title: 'Subheading', description: 'Body text for whatever you’d like to share more.', image: '/img/2147775681-2.jpg' }
+    { title: 'Subheading', description: 'Body text for whatever you’d like to add more to the subheading.', image: 'https://wallpapers.com/images/featured/movie-9pvmdtvz4cb0xl37.jpg' },
+    { title: 'Subheading', description: 'Body text for whatever you’d like to expand on the main point.', image: 'https://preview.redd.it/xe2175thl6u81.jpg?auto=webp&s=5842f2df12adb095e635f43e809d2f38093e7a13' },
+    { title: 'Subheading', description: 'Body text for whatever you’d like to share more.', image: 'https://cloudfront-us-east-1.images.arcpublishing.com/infobae/2NJO3FE7QBHQJD5GKNCI6XIODM.jpg' }
   ];
 
   currentNewsIndex = 0;

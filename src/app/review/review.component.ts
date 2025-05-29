9a004824ac370654909d3bf31c3f407bb2935ce9
EddyLucany <eddylucandy@icloud.com>
2025-05-29 20:56:38 +0200

    cambioos

--- src/app/review/review.component.ts
+++ src/app/review/review.component.ts
@@ -26,11 +26,10 @@ interface Movie {
 
 export class ReviewComponent {
 
-  movieTitle: string = 'Captain America brave new world';
-  movieDescription: string = 'Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut.';
-  trailerTime: string = '00:30';
-  rating: number = 4; // Assuming 4 out of 5 stars
-  reviewText: string = 'Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut.';
+  reviewItems = [
+    { title: 'Minecraft', description: 'Exceptional efficient emerging, minimum viable content curated...', image:'https://hips.hearstapps.com/hmg-prod/images/minecraft-pelicula-critica-67ef8f226988c.jpg?crop=1xw:1xh;center,top&resize=1200:*', rating: 4, trailerTime:'00:30', comment: 'Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut.'  }
+  ]
+
 
   stars: number[] = [1, 2, 3, 4, 5]; // For rendering stars
 
@@ -38,5 +37,12 @@ export class ReviewComponent {
 
   ngOnInit(): void {
   }
+/*
+  movieTitle: string = 'Captain America brave new world';
+  movieDescription: string = 'Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut.';
+  trailerTime: string = '00:30';
+  rating: number = 4; // Assuming 4 out of 5 stars
+  reviewText: string = 'Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut. Excepteur efficient emerging, minim veniam anim aute carefully curated Ginza conversation exquisite perfect nostrud nisi intricate Content. Qui international first-class nulla ut.';
+*/
 
 }
